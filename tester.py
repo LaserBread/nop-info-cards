6089cf8ab298bc39d354b447ae68aa71de4d9cc9
Ethan Jones <aaaa@aaaa.aaaa>
2024-03-03 23:10:29 -0800

    rteter

--- tester.py
+++ tester.py
@@ -5,8 +5,4 @@ with open("human.svg") as file:
     xml = file.read()
 
 root = etree.fromstring(bytes(xml,encoding = 'utf-8'))
-image = root.getroot()
-para = image.findall('svg')
-
-for e in para:
-    etree.dump(e)
+print(root.tag)
