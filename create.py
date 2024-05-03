97beb400a6bb1ffd99775e1a4866ec790dde420b
Ethan Jones <ej.riachu1@gmail.com>
2024-05-02 17:26:48 -0700

    Several changes were made:

--- create.py
+++ create.py
@@ -2,9 +2,13 @@
 from spreadsheet import datatable 
 from svg_generator import builder
 import subprocess
-
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
 table = datatable('/home/LaserBread/Downloads/NoP Infocards Master Table.xlsx')
 build = builder("./assets/template.svg")
 for sp in table.get_all():
     build.build(sp)
     subprocess.run(['inkscape',  '--export-filename=./export/'+sp.name+'.png',  "--export-area-page", 'tmp.svg'])
+    
+os.remove("./tmp.svg")
